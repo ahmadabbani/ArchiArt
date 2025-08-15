5cd1105515e15285af8386a678ea201b70566b83
ahmadabbani <ahmdabbanyy@gmail.com>
2025-08-15 14:11:39 +0300

    .

--- product.js
+++ product.js
@@ -195,14 +195,10 @@ document.addEventListener("DOMContentLoaded", function () {
   `
       : ""
   }
-  ${
-    product.branding
-      ? `
+  
     <span class="singleproduct_breadcrumb-separator">/</span>
    <span class="singleproduct_breadcrumb-current">${product.branding}</span>
-  `
-      : ""
-  }
+ 
   <span class="singleproduct_breadcrumb-separator">/</span>
   <span class="singleproduct_breadcrumb-current">${product.title}</span>
 `;
